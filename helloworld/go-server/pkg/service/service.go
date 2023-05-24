f7468cd0277d5db034579d19fc285f4fa5273bbe
Wesley Wu <wumengye@gmail.com>
2023-05-25 02:39:22 +0800

    support gcp traffic director deploying

--- helloworld/go-server/pkg/service/service.go
+++ helloworld/go-server/pkg/service/service.go
@@ -2,11 +2,15 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	api "dubbo-go-server/api"
 	"dubbo.apache.org/dubbo-go/v3/config"
 	"github.com/dubbogo/gost/log/logger"
 )
+
+var hostName string
 
 type GreeterServerImpl struct {
 	api.UnimplementedGreeterServer
@@ -14,9 +18,14 @@ type GreeterServerImpl struct {
 
 func (s *GreeterServerImpl) SayHello(ctx context.Context, in *api.HelloRequest) (*api.HelloReply, error) {
 	logger.Infof("Dubbo-go GreeterProvider get user name = %s\n", in.Name)
-	return &api.HelloReply{Message: "Hello " + in.Name}, nil
+	return &api.HelloReply{Message: fmt.Sprintf("Hello %s, from %s", in.Name, hostName)}, nil
 }
 
 func init() {
 	config.SetProviderService(&GreeterServerImpl{})
+	var err error
+	hostName, err = os.Hostname()
+	if err != nil {
+		panic(err)
+	}
 }
--- helloworld/python-client/main.py
+++ helloworld/python-client/main.py
@@ -17,7 +17,6 @@ import logging
 import os
 from contextlib import asynccontextmanager
 
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from grpc import Channel, insecure_channel
 from uvicorn import Server, Config
@@ -27,7 +26,15 @@ from proto.helloworld_pb2_grpc import GreeterStub
 
 channel: Channel
 stub: GreeterStub
-greeter_service_endpoint = ""
+service_endpoint = ""
+xds_bootstrap_json_path = "xds_bootstrap.json"
+
+
+#
+# # Import the message definitions
+# from envoy.service.status.v3 import csds_pb2
+# # Import the gRPC service and stub
+# from envoy.service.status.v3 import csds_pb2_grpc
 
 
 @asynccontextmanager
@@ -39,23 +46,60 @@ async def lifespan(app: FastAPI):
     shutdown()
 
 
+def generate_xds_bootstrap(pod_ip, pod_name, pod_namespace):
+    # read input file
+    fin = open(xds_bootstrap_json_path, "rt")
+    # read file contents to string
+    data = fin.read()
+    # replace all occurrences of the required string
+    data = data.replace('{POD_IP}', pod_ip)
+    if pod_name is not None:
+        data = data.replace('{POD_NAME}', pod_name)
+    if pod_namespace is not None:
+        data = data.replace('{POD_NAMESPACE}', pod_namespace)
+    # close the input file
+    fin.close()
+    # open the input file in write mode
+    fin = open(xds_bootstrap_json_path, "wt")
+    # overwrite the input file with the resulting data
+    fin.write(data)
+    # close the file
+    fin.close()
+
+
 def startup():
-    global channel, stub, greeter_service_endpoint
-    try:
-        greeter_service_endpoint = os.environ['GREETER_SERVICE_ENDPOINT']
-    except KeyError:
-        load_dotenv()
-        greeter_service_endpoint = os.environ['GREETER_SERVICE_ENDPOINT']
-    channel = insecure_channel(greeter_service_endpoint)
-    stub = GreeterStub(channel)
     logging.basicConfig(encoding='utf-8', level=logging.INFO)
-    logging.info(f'Opened rpc {greeter_service_endpoint} channel ...')
+    global channel, stub, service_endpoint
+
+    service_endpoint = os.environ.get('SERVICE_ENDPOINT')
+
+    if service_endpoint is None:
+        raise Exception("SERVICE_ENDPOINT not set")
+
+    pod_ip = os.environ.get('POD_IP')
+    pod_name = os.environ.get('POD_NAME')
+    pod_namespace = os.environ.get('POD_NAMESPACE')
+    logging.info(f'service_endpoint: {service_endpoint}, '
+                 f'pod_ip: {pod_ip}, '
+                 f'pod_name: {pod_name}, '
+                 f'pod_namespace: {pod_namespace}')
+
+    # check if k8s deployment
+    if pod_ip is not None:
+        # replace envoy xds bootstrap file content
+        generate_xds_bootstrap(pod_ip, pod_name, pod_namespace)
+        channel = insecure_channel(service_endpoint)
+    else:
+        # calling local endpoint
+        channel = insecure_channel(service_endpoint)
+    stub = GreeterStub(channel)
+    logging.info(f'Opened rpc {service_endpoint} channel ...')
 
 
 def shutdown():
-    global channel, stub, greeter_service_endpoint
+    global channel, stub, service_endpoint
     if channel:
-        logging.info(f"Closing rpc {greeter_service_endpoint} channel")
+        logging.info(f"Closing rpc {service_endpoint} channel")
         channel.close()
 
 
